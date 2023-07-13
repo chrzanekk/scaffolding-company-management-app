273e2bae28f1fc59229d911a0928a3ec084a2cb2
Konrad <chrzanekk@gmail.com>
2023-07-13 07:02:51 +0200

    #2 working on fixing date formatting

--- scaffolding-company-management-app/src/main/java/pl/com/chrzanowski/scma/ScaffoldingCompanyManagementAppApplication.java
+++ scaffolding-company-management-app/src/main/java/pl/com/chrzanowski/scma/ScaffoldingCompanyManagementAppApplication.java
@@ -2,17 +2,10 @@ package pl.com.chrzanowski.scma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class ScaffoldingCompanyManagementAppApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ScaffoldingCompanyManagementAppApplication.class, args);
--- scaffolding-company-management-app/src/main/java/pl/com/chrzanowski/scma/config/ApplicationConfig.java
+++ scaffolding-company-management-app/src/main/java/pl/com/chrzanowski/scma/config/ApplicationConfig.java
@@ -1,16 +1,15 @@
 package pl.com.chrzanowski.scma.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
+
 @Configuration
 @EnableCaching
 @EnableScheduling
@@ -198,10 +197,5 @@ public class ApplicationConfig {
         return restTemplate;
     }
 
-    @Bean
-    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-        return builder;
-    }
+
 }
